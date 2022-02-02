4081d5cb1ecea1b7e9186419bbbecf8f2b909cb4
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2022-02-02 22:15:54 +0900

    Update Baekjoon_5576.cpp

--- Baekjoon_5576.cpp
+++ Baekjoon_5576.cpp
@@ -12,7 +12,7 @@ int main(void) {
         scanf("%d", &K[i]);
     sort(K, K + 10);
     int sum_w = 0, sum_k = 0;
-    for (int i = 9; i > 6; i--) // 상위 3명의 점수
+    for (int i = 7; i < 10; i++) // 상위 3명의 점수
         sum_w += W[i], sum_k += K[i];
     printf("%d %d\n", sum_w, sum_k);
     return 0;
