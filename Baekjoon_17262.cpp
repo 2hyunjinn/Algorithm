8f28b34387d8d54d73947948efac3b4ad9412cb1
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2022-02-03 16:06:56 +0900

    Update Baekjoon_17262.cpp

--- Baekjoon_17262.cpp
+++ Baekjoon_17262.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n;
+int n, max_v, min_v;
 int first[100000], second[100000];
 /* 팬의 수, 학교에 있는 시간 입력 */
 void input_info() {
@@ -11,16 +11,16 @@ void input_info() {
 		scanf("%d%d", &first[i], &second[i]);
 	}
 }
-/* 시간 정렬 */
-void sort_arr() {
-	sort(first, first + n); // 최소 이때부터는 있어야 함
-	sort(second, second + n); // 최대 이때까지는 있어야 함
+/* 시작, 끝 시간 구하기 */
+void get_time() {
+	min_v = *max_element(first, first + n); // 최소 이때부터는 있어야 함
+	max_v = *min_element(second, second + n); // 최대 이때까지는 있어야 함
 }
 /* 최소 시간 구하기 */
 int get_mintime() {
-	sort_arr();
-	if ((first[n - 1] - second[0]) < 0) return 0;
-	else return (first[n - 1] - second[0]);
+	get_time();
+	if (min_v - max_v < 0) return 0;
+	else return (min_v - max_v);
 }
 int main() {
 	input_info();
