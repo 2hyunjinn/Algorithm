3a9ddcc9389686da8bdbac775f8c2d299b9320d6
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2022-07-17 10:12:00 +0900

    Create Baekjoon_25083.cpp

---
+++ Baekjoon_25083.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+
+	return 0;
+}
