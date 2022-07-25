36ec6356ae2d13f3130314e41fc281cb4c0048c3
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2022-07-25 11:10:31 +0900

    Update Baekjoon_2444.cpp

--- Baekjoon_2444.cpp
+++ Baekjoon_2444.cpp
@@ -6,12 +6,13 @@ int main(void)
 {
     int n;
     scanf("%d", &n);
+    
     for (int i = 1; i <= n * 2 - 1; i++) {
         for (int j = 1; j <= abs(n - i); j++) {
             printf(" ");
         }
-        int x = (i <= n) ? (i * 2 - 1) : ((n - (i % n)) * 2 - 1);
-        for (int j = 1; j <= x; j++) {
+
+        for (int j = 1; j <= (n - abs(n - i)) * 2 - 1; j++) {
             printf("*");
         }
         printf("\n");
