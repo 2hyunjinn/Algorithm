36d01b6c5830b1100e5e4536c04624561c3c47fa
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2022-07-26 09:37:27 +0900

    Create Baekjoon_4375.cpp

---
+++ Baekjoon_4375.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    while (cin >> n) {
+        int tmp = 0;
+        for (int i = 1; i <= n; i++) {
+            tmp = tmp * 10 + 1;
+            tmp %= n;
+            if (tmp == 0) {
+                cout << i << "\n";
+                break;
+            }
+        }
+    }
+}
