763cb45bff41e461210eabe7df6c46d2e8bcfac6
2hyunjinn <95455569+2hyunjinn@users.noreply.github.com>
2022-07-28 18:04:18 +0900

    Create Baekjoon_5622.cpp

---
+++ Baekjoon_5622.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s;
+    cin >> s;
+
+    int time = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= 'A' && s[i] <= 'C') time += 3;
+        else if (s[i] >= 'D' && s[i] <= 'F') time += 4;
+        else if (s[i] >= 'G' && s[i] <= 'I') time += 5;
+        else if (s[i] >= 'J' && s[i] <= 'L') time += 6;
+        else if (s[i] >= 'M' && s[i] <= 'O') time += 7;
+        else if (s[i] >= 'P' && s[i] <= 'S') time += 8;
+        else if (s[i] >= 'T' && s[i] <= 'V') time += 9;
+        else if (s[i] >= 'W' && s[i] <= 'Z') time += 10;
+    }
+    cout << time << '\n';
+    return 0;
+}
